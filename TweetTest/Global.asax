6d05abcf000b797c7e3ea450aacb963936ac5f7d
lycheejam <lycheejamwow@gmail.com>
2018-01-02 00:23:27 +0900

    プロジェクト ファイルを追加します。

---
+++ TweetTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TweetTest.MvcApplication" Language="C#" %>
---
+++ TweetTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TweetTest.Startup))]
+namespace TweetTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
