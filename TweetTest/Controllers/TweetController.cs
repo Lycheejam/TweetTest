e2bb90369548cc8fc97d30d34fbd10538d0cb164
lycheejam <lycheejamwow@gmail.com>
2018-01-06 15:19:59 +0900

    ツイートレスポンスを受け取るようにした

--- TweetTest/Controllers/TweetController.cs
+++ TweetTest/Controllers/TweetController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,9 +19,11 @@ namespace TweetTest.Controllers
 
         public ActionResult TweetPost(TweetViewModels tt)
         {
+            CoreTweet.StatusResponse res;
             var tokens = CoreTweet.Tokens.Create("API-Key", "API-Secret", "AccessToken", "AccessTokenSecret");
-            tokens.Statuses.Update(status => DateTime.Now + " " + tt.TweetText);
-            return View(tt);
+            res = tokens.Statuses.Update(status => DateTime.Now + " " + tt.TweetText);
+            Debug.Print(res.Text);
+            return View(res);
         }
     }
 }
