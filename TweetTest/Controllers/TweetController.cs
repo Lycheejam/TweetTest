209e4d4fe7beb18c961af2ecd89cc06a06676e9f
lycheejam <lycheejamwow@gmail.com>
2018-01-16 22:38:25 +0900

    ツイートトークン生成をメソッド化 debugでコード追ってみたけどASP.NETが何やってるかわからない

--- TweetTest/Controllers/TweetController.cs
+++ TweetTest/Controllers/TweetController.cs
@@ -21,13 +21,7 @@ namespace TweetTest.Controllers {
 
         //テーブルを参照する際、awaitを使用しているのでasyncに変更？
         public async Task<ActionResult> TweetPost(TweetViewModels tt) {
-            GetToken at = await GetClaimTokens();
-            var keys = MyTokens;
-
-            var tokens = CoreTweet.Tokens.Create(keys.ConsumerKey
-                                               , keys.ConsumerSecret
-                                               , at.accessToken    //テーブルから参照
-                                               , at.accessTokenSecret);    //テーブルから参照
+            var tokens = await CreateTokens();
             //ツイート後、レスポンス取得
             var res = tokens.Statuses.Update(status => DateTime.Now + " " + tt.TweetText);
             Debug.Print(res.Text);
@@ -54,16 +48,8 @@ namespace TweetTest.Controllers {
         }
         [HttpPost]
         public async Task<ActionResult> Reply(string TweetText, long replyId) {
-            //ここを共通化したいんだけどどうすんだ
-
-
-            GetToken at = await GetClaimTokens();
-            var keys = MyTokens;
-
-            var tokens = CoreTweet.Tokens.Create(keys.ConsumerKey
-                                               , keys.ConsumerSecret
-                                               , at.accessToken    //テーブルから参照
-                                               , at.accessTokenSecret);    //テーブルから参照
+            //ツイート用トークン生成
+            var tokens = await CreateTokens();
             //ツイート後、レスポンス取得
             var res = tokens.Statuses.Update(status => DateTime.Now + " " + TweetText
                                             , in_reply_to_status_id => replyId);
@@ -80,24 +66,38 @@ namespace TweetTest.Controllers {
             return View("TweetPost", tr);
         }
 
-        //コピペ AccessToken&Secretをテーブルから参照する。
+        /*
+         * javaみたいにセッションスコープとかインスタンス保存しておくものってないのかな？
+         * この生成されたトークンを使いまわすみたいに思っていたんだけどできない？
+         */
+
+        //claimテーブルの参照はコピペ AccessToken&Secretをテーブルから参照する。
         //コピペ元 » ASP.NET Identity：Twitter認証時の情報でツイートする方法 - なか日記 
         // http://blog.nakajix.jp/entry/2014/09/12/074000
-        public async Task<GetToken> GetClaimTokens() {
-            var token = new GetToken();
+        public async Task<Tokens> CreateTokens() {
+            var claimkeys = new GetToken();
+
+            //httpcontextクラス httpリクエストに反応していろいろしてくれるらしい
             var usermgr = HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
             var claims = await usermgr.GetClaimsAsync(User.Identity.GetUserId());
-
-
 
             var firstOrDefault = claims.FirstOrDefault(x => x.Type == "ExternalAccessToken");
             if (firstOrDefault != null) // TokenとTokenSecretはペアで登録されるのでnullチェックは片方のみ行う
             {
-                token.accessToken = firstOrDefault.Value;
-                token.accessTokenSecret = claims.FirstOrDefault(x => x.Type == "ExternalAccessTokenSecret").Value;
+                claimkeys.accessToken = firstOrDefault.Value;
+                claimkeys.accessTokenSecret = claims.FirstOrDefault(x => x.Type == "ExternalAccessTokenSecret").Value;
             }
+
+            //Models.ReadToken APIキー取ってきてる。
+            var keys = MyTokens;
+
+            //ツイート用トークン生成
+            var tokens = Tokens.Create(keys.ConsumerKey
+                                               , keys.ConsumerSecret
+                                               , claimkeys.accessToken    //テーブルから参照
+                                               , claimkeys.accessTokenSecret);    //テーブルから参照
             //コピペここまで
-            return token;
+            return tokens;
         }
     }
 }
