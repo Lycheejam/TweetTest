f1c2a2aec1f34611479cda20f6e0b0d32fd3baae
lycheejam <lycheejamwow@gmail.com>
2018-04-24 23:38:04 +0900

    DBコネクションの作成をクラス全体ではなく各メソッドで行うように変更 各メソッドにtrycatchとusingを実装

--- TweetTest/Models/TaskStoreManager.cs
+++ TweetTest/Models/TaskStoreManager.cs
@@ -9,8 +9,6 @@ using System.Data.Entity;
 
 namespace TweetTest.Models {
     public class TaskStoreManager {
-        private MyContext db = new MyContext();
-
         //タスクの初回登録
         public int CreateTask(TweetResult tr) {
             var tresult = new TweetResult {
@@ -19,8 +17,10 @@ namespace TweetTest.Models {
                 tweetId = tr.tweetId
             };
             try {
-                db.TweetResults.Add(tr);
-                db.SaveChanges();
+                using (MyContext db = new MyContext()) {
+                    db.TweetResults.Add(tr);
+                    db.SaveChanges();
+                }
                 return 0;   //正常終了値のつもり、あとでちゃんと考えようね
             } catch (Exception) {
                 throw;
@@ -28,40 +28,52 @@ namespace TweetTest.Models {
         }
 
         public TweetResult ReadTask(string id) {
-            //データ消さない方針ならendflag別にいらなくね？
-            TweetResult tr = db.TweetResults.Where(x => x.userId == id && x.endFlag == 0)
-                                            .Include("MyTasks")
-                                            .SingleOrDefault();
-            return tr;  //最後に見つかったレコードは必ずendFlagが0?
+            try {
+                using (MyContext db = new MyContext()) {
+                    //データ消さない方針ならendflag別にいらなくね？
+                    TweetResult tr = db.TweetResults.Where(x => x.userId == id && x.endFlag == 0)
+                                                    .Include("MyTasks")
+                                                    .SingleOrDefault();
+
+                    return tr;  //最後に見つかったレコードは必ずendFlagが0?
+                }
+            } catch (Exception) {
+                throw;
+            }
         }
 
         //タスクのステータス更新とツイートID（リプライ先）の更新
         public int UpdateTask(TweetResult tr) {
-            var tresult = db.TweetResults.Where(x => x.id == tr.id)
-                                         .Include("MyTasks")
-                                         .SingleOrDefault();
-            tresult = tr;
             try {
-                db.SaveChanges();
-                return 0;   //正常終了値のつもり、あとでちゃんと考えようね
+                using (MyContext db = new MyContext()) {
+                    var tresult = db.TweetResults.Where(x => x.id == tr.id)
+                                             .Include("MyTasks")
+                                             .SingleOrDefault();
+                    tresult = tr;
+                    db.SaveChanges();
+                    return 0;   //正常終了値のつもり、あとでちゃんと考えようね
+                }
             } catch (Exception) {
                 throw;
             }
         }
 
+
         //タスクの削除（と言うかエンドフラグを立てて表示させないようにする。）
         public int DeleteTask(string id) {
             try {
-                TweetResult tr = db.TweetResults.Where(x => x.userId == id && x.endFlag == 0)
-                                                .Include("MyTasks")
-                                                .SingleOrDefault();
-                tr.endFlag = 1;
-                db.SaveChanges();
-                return 0;
+                using (MyContext db = new MyContext()) {
+
+                    TweetResult tr = db.TweetResults.Where(x => x.userId == id && x.endFlag == 0)
+                                                    .Include("MyTasks")
+                                                    .SingleOrDefault();
+                    tr.endFlag = 1;
+                    db.SaveChanges();
+                    return 0;
+                }
             } catch (Exception) {
                 throw;
             }
-            
         }
     }
 }
